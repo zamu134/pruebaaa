7e7b5d37b2350aaeddadf747297d14e166aa10de
zamu134 <117544767+zamu134@users.noreply.github.com>
2023-10-15 15:12:42 -0600

    Add files via upload

---
+++ consulta a la base de dato/EstudianteUNAB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consulta_a_la_base_de_dato
+{
+ public class EstudianteUNAB
+    {
+            public int? Id { get; set; }
+
+            public string Nombre { get; set; }
+
+            public string Apellido { get; set; }
+
+            public int Edad { get; set;}
+
+            public string Sexo { get; set; }
+
+    }
+}
---
+++ consulta a la base de dato/Program.cs
@@ -0,0 +1,71 @@
+﻿using consulta_a_la_base_de_dato;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            using (var context = new Context())
+            {
+                Console.WriteLine("Lista de Estudiantes:");
+                foreach (var estudiante in context.Estudiante.ToList())
+                {
+                    Console.WriteLine($"ID: {estudiante.Id}, Nombre: {estudiante.Nombre}, Edad: {estudiante.Edad}, Sexo: {estudiante.Sexo}");
+                }
+            }
+
+            Console.WriteLine("\nIngrese los datos de los nuevos estudiantes:");
+
+            Console.Write("Nombre: ");
+            var nombre = Console.ReadLine();
+
+            Console.Write("Apellido: ");
+            var apellido = Console.ReadLine();
+
+            Console.Write("Sexo: ");
+            var sexo = Console.ReadLine();
+
+            Console.Write("Edad: ");
+            if (int.TryParse(Console.ReadLine(), out int edad))
+            {
+                var nuevoEstudiante = new EstudianteUNAB
+                {
+                    Nombre = nombre,
+                    Apellido = apellido,
+                    Sexo = sexo,
+                    Edad = edad
+                };
+
+                using (var context = new Context())
+                {
+                    try
+                    {
+                        context.Estudiante.Add(nuevoEstudiante);
+                        context.SaveChanges();
+                        Console.WriteLine("Estudiante agregado correctamente.");
+                    }
+                    catch (DbUpdateException ex)
+                    {
+                        Console.WriteLine("Error al agregar estudiante. Asegúrate de que los datos sean válidos.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Edad no válida. Intente nuevamente.");
+            }
+
+            Console.Write("¿Si de sea agregar mas estudiante presione (S) y si ya no de sea ingresar presione (N): ");
+            var respuesta = Console.ReadLine();
+            if (respuesta?.Trim().ToLower() != "s")
+            {
+                break;
+            }
+        }
+    }
+}
